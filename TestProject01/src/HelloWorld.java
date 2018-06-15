687588bf3a58635623203cf30929d498150b4394
kousukekumagai <kumakou8@icloud.com>
2018-06-15 09:49:52 +0900

    0614

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello World!の出力
+
+		int i = 10;
+		do{
+			System.out.println(i);
+			i-=3;
+		} while(i >= 1);
+
+
+
+		}
+}
